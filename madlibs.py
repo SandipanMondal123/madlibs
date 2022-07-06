aa1f23e40c7ec0f57a19b3d2aaf58b9e0fb20927
Sandipan Mondal <office.smondal@gmail.com>
2022-07-05 23:29:49 -0400

    Created a simple madlib

--- madlibs.py
+++ madlibs.py
@@ -1,6 +1,15 @@
-temp = "amazing person!"
+# temp = "amazing person!"
 
-print("You are an " + temp)
-print("You are an %s" %temp)
-print("You are an {}".format(temp))
-print(f"You are an {temp}")
+# print("You are an " + temp)
+# print("You are an %s" %temp)
+# print("You are an {}".format(temp))
+# print(f"You are an {temp}")
+
+adj = input("Adjective: ")
+verb = input("Verb: ")
+noun = input("Noun: ")
+
+madlib = f"Wow, that was a {adj} cruise ship. Nonetheless, I am happy that I made it to the \
+    destination and can't wait to {verb}. Anyway, I better head to the luggage pickup and retrieve my {noun}."
+
+print(madlib)
