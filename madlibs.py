58df4abeb2c50734ea9ad756b901781446d7110b
Sandipan Mondal <office.smondal@gmail.com>
2022-07-05 23:18:46 -0400

    testing different ways to concatenate

--- madlibs.py
+++ madlibs.py
@@ -1,2 +1,6 @@
-print("hello")
-print("Another print to test git")
+temp = "amazing person!"
+
+print("You are an " + temp)
+print("You are an %s" %temp)
+print("You are an {}".format(temp))
+print(f"You are an {temp}")
